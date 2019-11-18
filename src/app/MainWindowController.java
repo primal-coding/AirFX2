65266d5e1011f373058417a052085a70aac2a068
primal-coding <primal.coding@gmail.com>
2019-11-18 18:53:48 +0000

    Initial commit

---
+++ src/app/MainWindowController.java
@@ -0,0 +1,23 @@
+package app;
+
+public class MainWindowController {
+
+  private Main main;
+
+  public void setMain(){
+    this.main = main;
+  }
+
+  public void showAlert1(){
+
+  }
+  public void showAlert2(){
+
+  }
+  public void showAlert3(){
+
+  }
+  public void showAlert4(){
+
+  }
+}
