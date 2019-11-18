476992228b2e2ce1737956cfdc218910011f0056
primal-coding <primal.coding@gmail.com>
2019-11-18 19:51:18 +0000

    Final commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,16 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private Stage  primaryStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindowView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        this.primaryStage = primaryStage;
+        mainWindow();
+    }
+
+    public void mainWindow(){
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainWindowView.fxml"));
+            AnchorPane pane = loader.load();
+
+            MainWindowController mainWindowController = loader.getController();
+            mainWindowController.setMain(this);
+            Scene scene = new Scene(pane);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/app/MainWindowController.java
+++ src/app/MainWindowController.java
@@ -1,21 +1,49 @@
 package app;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
+
 public class MainWindowController {
 
   private Main main;
 
-  public void setMain(){
+  public void setMain(Main main){
     this.main = main;
   }
 
   public void showAlert1(){
-
+    Alert alert = new Alert(Alert.AlertType.INFORMATION); // error, warning, confirmation(cancel + ok)
+    alert.setTitle("This");
+    alert.setHeaderText("In for mation hEADER.");
+    alert.setContentText("This is the content.");
+    alert.showAndWait();
   }
   public void showAlert2(){
-
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION); // error, warning, confirmation(cancel + ok)
+    alert.setTitle("This Confirmation");
+    alert.setHeaderText("Question mark?!.");
+    alert.setContentText("Make your choice:");
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.get() == ButtonType.OK){
+      System.out.println("Button Clicked oK.");
+    }
+    else {
+      System.out.println("Button Clicked Cancel.");
+    }
   }
   public void showAlert3(){
-
+    TextInputDialog dialog = new TextInputDialog();
+    dialog.setTitle("Question mark icon!?.");
+    dialog.setContentText("Input your text:");
+    Optional<String> result = dialog.showAndWait();
+    if (result.isPresent()) {
+      System.out.println("Your input: " + result.get());
+    } else {
+      System.out.println("Your Cancel!" );   // + result.get() would crash the prog lol
+    }
   }
   public void showAlert4(){
 
